e6c112e2ea6e9d28d549539b3b81885072caee1d
Huy Nguyen <h.nguyen.2590@gmail.com>
2017-11-25 23:25:13 -0700

    Created simple spring-boot application and initiate angular4 frontend

---
+++ tasks/src/main/java/com/therealdanvega/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.therealdanvega.controller;
+
+import com.therealdanvega.domain.Task;
+import com.therealdanvega.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/tasks")
+public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping(value = {"", "/"})
+    public Iterable<Task> list() {
+        return this.taskService.list();
+    }
+
+    @PostMapping(value = "/save")
+    public Task saveTask(@RequestBody Task task) {
+        return this.taskService.save(task);
+    }
+
+}
